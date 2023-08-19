ffde725dc6065b2e13f6f34a18905dd83a922903
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-08-19 22:21:49 +0900

    Timeover

--- BackJoon_Archive/BackJoon_Archive/Program.cs
+++ BackJoon_Archive/BackJoon_Archive/Program.cs
@@ -10,6 +10,48 @@ namespace BackJoon_Archive
     {
         static void Main(string[] args)
         {
+            string prob;
+            prob = Console.ReadLine();
+            int max_count = -1;
+            char ans = prob[0];
+            for (int i = 0; i < prob.Length; i++)
+			{                    
+                int count = 0;
+                int a = prob[i];
+
+                if(a > 91)
+                {
+                    a = a - 32;
+                }
+
+                for (int j = i+1; j < prob.Length; j++)
+			    {
+                    int b = prob[j];
+
+                    if(b > 91)
+                    {
+                        b = b - 32;
+                    }
+
+                    if(a == b)
+                    {
+                        ++count;
+                    }
+
+			    }
+
+                if (count > max_count)
+	            {
+                    max_count = count;
+                    ans = prob[i];
+	            }
+
+                else if (count == max_count)
+                {
+                    ans = '?';
+                }
+			}
+            Console.WriteLine(ans.ToString().ToUpper());
         }
     }
 }
