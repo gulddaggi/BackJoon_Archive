9fca57253073b1ac13fdd680d34998150cd5aa92
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-08-22 17:32:51 +0900

    2577

--- BackJoon_Archive/BackJoon_Archive/Program.cs
+++ BackJoon_Archive/BackJoon_Archive/Program.cs
@@ -10,6 +10,31 @@ namespace BackJoon_Archive
     {
         static void Main(string[] args)
         {
+            int[] num = new int[10];
+            int tmp = 1;
+
+            for (int i = 0; i < num.Length; i++)
+			{
+                num[i] = 0;
+			}
+
+            for (int i = 0; i < 3; i++)
+			{
+                tmp *= int.Parse(Console.ReadLine());
+			}
+
+            string ans = tmp.ToString();
+
+            for (int i = 0; i < ans.Length; i++)
+			{
+                int index = int.Parse(ans[i].ToString());
+                num[index] += 1;
+			}
+
+            for (int i = 0; i < num.Length; i++)
+			{
+                Console.WriteLine(num[i]);
+			}
         }
     }
 }
