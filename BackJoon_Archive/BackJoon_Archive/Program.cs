bc99463b8e059151fea6e1a16993f0577e48a27b
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-08-26 01:14:44 +0900

    2753

--- BackJoon_Archive/BackJoon_Archive/Program.cs
+++ BackJoon_Archive/BackJoon_Archive/Program.cs
@@ -10,6 +10,16 @@ namespace BackJoon_Archive
     {
         static void Main(string[] args)
         {
+            int year = int.Parse(Console.ReadLine());
+
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
         }
     }
 }
