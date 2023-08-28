cf157ab4919926d08c7342b56fea513c2683c18c
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-08-28 20:29:53 +0900

    10818

--- BackJoon_Archive/BackJoon_Archive/Program.cs
+++ BackJoon_Archive/BackJoon_Archive/Program.cs
@@ -10,6 +10,27 @@ namespace BackJoon_Archive
     {
         static void Main(string[] args)
         {
+            uint count = uint.Parse(Console.ReadLine());
+            string[] prob = Console.ReadLine().Split();
+            int min = int.Parse(prob[0]);
+            int max = int.Parse(prob[0]);
+
+            for (int i = 0; i < count; i++)
+            {
+                int tmp = int.Parse(prob[i]);
+
+                if (tmp > max)
+                {
+                    max = tmp;
+                }
+
+                if (tmp < min)
+                {
+                    min = tmp;
+                }
+            }
+            
+            Console.Write(min + " " + max);
         }
     }
 }
