e0ad072676e80ea715ce92a1d32266b7a01a5809
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-08-30 17:58:55 +0900

    3052

--- BackJoon_Archive/BackJoon_Archive/Program.cs
+++ BackJoon_Archive/BackJoon_Archive/Program.cs
@@ -10,6 +10,43 @@ namespace BackJoon_Archive
     {
         static void Main(string[] args)
         {
+            int[] nums = new int[10];
+            List<int> sameList = new List<int>();
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+			{
+                nums[i] = int.Parse(Console.ReadLine()) % 42;
+			}
+
+            for (int i = 0; i < nums.Length; i++)
+			{
+                int tmp = count;
+
+                if (sameList.Contains(nums[i]))
+                {
+                    continue;
+                }
+                for (int j = 1; j < nums.Length; j++)
+	    		{
+                    if (nums[i] != nums[j])
+                    {
+                        ++count;
+                        sameList.Add(nums[i]);
+                        break;
+                    }
+    			}
+
+                if(tmp != count)
+                {
+                    continue;
+                }
+			}
+
+            if(count == 0)
+            {
+                ++count;
+            }
+            Console.WriteLine(count);
         }
     }
 }
