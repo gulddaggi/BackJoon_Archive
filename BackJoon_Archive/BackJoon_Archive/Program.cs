5c8311e62ba7eb9b7e49dfe0945f30e02fcf8b6b
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-08-30 23:54:54 +0900

    10172

--- BackJoon_Archive/BackJoon_Archive/Program.cs
+++ BackJoon_Archive/BackJoon_Archive/Program.cs
@@ -10,6 +10,11 @@ namespace BackJoon_Archive
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("|\\_/|");
+            Console.WriteLine("|q p|   /}");
+            Console.WriteLine("( 0 )\"\"\"\\");
+            Console.WriteLine("|\"^\"`    |");
+            Console.WriteLine("||_/=\\\\__|");
         }
     }
 }
