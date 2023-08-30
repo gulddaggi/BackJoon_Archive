f811a97b3ca51b1164d8b7a5cd913e6b718541a4
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-08-30 17:24:10 +0900

    2884

--- BackJoon_Archive/BackJoon_Archive/Program.cs
+++ BackJoon_Archive/BackJoon_Archive/Program.cs
@@ -10,6 +10,16 @@ namespace BackJoon_Archive
     {
         static void Main(string[] args)
         {
+            string[] time = Console.ReadLine().Split(' ');
+            int hour = int.Parse(time[0]);
+            int minute = int.Parse(time[1]);
+            minute -= 45;
+            if (minute < 0)
+            {
+                if(--hour == -1) hour = 23;
+                minute += 60;
+            }
+            Console.WriteLine(hour.ToString() + " " + minute.ToString());
         }
     }
 }
