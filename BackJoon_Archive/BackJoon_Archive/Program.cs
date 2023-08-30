3ecc38b5acfbaac9c7a53450e112045b982be5a1
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-08-30 17:34:44 +0900

    2920

--- BackJoon_Archive/BackJoon_Archive/Program.cs
+++ BackJoon_Archive/BackJoon_Archive/Program.cs
@@ -10,6 +10,37 @@ namespace BackJoon_Archive
     {
         static void Main(string[] args)
         {
+            string[] prob = Console.ReadLine().Split(' ');
+            if(int.Parse(prob[0]) == 1)
+            {
+                int thr = 1;
+                for (int i = 1; i < prob.Length; i++)
+			    {
+                    if (int.Parse(prob[i]) != ++thr)
+                    {
+                        Console.WriteLine("mixed");
+                        return;
+                    }
+			    }
+                Console.WriteLine("ascending");
+            }
+            else if(int.Parse(prob[0]) == 8)
+            {
+                int thr = 8;
+                for (int i = 1; i < prob.Length; i++)
+			    {
+                    if (int.Parse(prob[i]) != --thr)
+                    {
+                        Console.WriteLine("mixed");
+                        return;
+                    }
+			    }
+                Console.WriteLine("descending");
+            }
+            else
+            {
+                Console.WriteLine("mixed");
+            }
         }
     }
 }
