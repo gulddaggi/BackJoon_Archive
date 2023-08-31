73996769be8c098e701b41c95752202f0e16669e
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-08-31 14:48:06 +0900

    10871

--- BackJoon_Archive/BackJoon_Archive/Program.cs
+++ BackJoon_Archive/BackJoon_Archive/Program.cs
@@ -10,6 +10,19 @@ namespace BackJoon_Archive
     {
         static void Main(string[] args)
         {
+            string[] prob = Console.ReadLine().Split(' ');
+            string[] prob_num = Console.ReadLine().Split(' ');
+            int[] nums = new int[int.Parse(prob[0])];
+            int thr = int.Parse(prob[1]);
+            for (int i = 0; i < nums.Length; i++)
+			{
+                nums[i] = int.Parse(prob_num[i]);
+                if (nums[i] < thr)
+                {
+                    Console.Write(nums[i]);
+                    Console.Write(" ");
+                }
+			}
         }
     }
 }
