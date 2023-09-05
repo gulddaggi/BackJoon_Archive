3a8b0f925080e341569b2c8242ac1a0312c449bb
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-09-05 09:23:22 +0900

    1181

--- BackJoon_Archive/BackJoon_Archive/Program.cs
+++ BackJoon_Archive/BackJoon_Archive/Program.cs
@@ -12,54 +12,45 @@ namespace BackJoon_Archive
         {
             int count = int.Parse(Console.ReadLine());
             string[] prob = new string[count];
-            string[] ans = new string[count];
+
             for (int i = 0; i < count; i++)
 			{
                 prob[i] = Console.ReadLine();
 			}
-            List<string> minList = new List<string>();
-            for (int i = 0; i < prob.Length; i++)
-			{
-                minList.Add(minList[i]);
 
-                for (int j = i+1; j < length; j++)
+            Array.Sort(prob);
+            List<string> tmp = prob.ToList();
+            List<string> ans = new List<string>();
+            List<string> minList = new List<string>();
+
+            while(tmp.Count > 0)
+            {
+                minList.Add(tmp[0]);
+                for (int i = 0; i < tmp.Count; i++)
 			    {
-                    if (minList[0].Length > prob[j].Length)
+                    if (tmp[i].Length < minList[0].Length)
                     {
                         minList.Clear();
-                        minList.Add(prob[j]);
+                        minList.Add(tmp[i]);
                     }
-                    else if(minList[0].Length == prob[j].Length)
+                    else if(tmp[i].Length == minList[0].Length)
                     {
-                        if (minList[0])
-                        {
-
-                        }
-                        for (int k = 0; k < minList[0].Length; k++)
-			            {
-                            // 중복 삭제
-
-                            // 추가
-                            if(minList[0][k] < prob[j][k])
-                            {
-                                minList.Add(prob[j]);
-                            }
-                            // 위치 변경
-                            else if (minList[0][k] > prob[j][k])
-                            {
-                                minList.Sort
-                            }
-			            }
+                        minList.Add(tmp[i]);
                     }
-                    
     			}
 
-                for (int x = 0; x < minList.Count; x++)
-			    {
-                    ans[i + x] = minList[x];
-			    }
-                minList.Clear();
+                for (int i = 0; i < minList.Count; i++)
+		    	{
+                    ans.Add(minList[i]);
+                    tmp.Remove(minList[i]);
+	    		}
 
+                minList.Clear();
+            }
+            ans = ans.Distinct().ToList();
+            for (int i = 0; i < ans.Count; i++)
+			{
+                Console.WriteLine(ans[i]);
 			}
         }
     }
