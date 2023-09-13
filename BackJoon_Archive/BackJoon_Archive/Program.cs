203012bb8d7cdc689de8b3215a0ab5947ad814b5
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-09-13 18:23:37 +0900

    13300

--- BackJoon_Archive/BackJoon_Archive/Program.cs
+++ BackJoon_Archive/BackJoon_Archive/Program.cs
@@ -10,6 +10,41 @@ namespace BackJoon_Archive
     {
         static void Main(string[] args)
         {
+            string[] prob = Console.ReadLine().Split();
+            int num = int.Parse(prob[0]);
+            int maximum = int.Parse(prob[1]);
+            int[,] stu_list = new int[6, 2];
+            int ans = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    stu_list[i, j] = 0;
+                }
+            }
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] tmp = Console.ReadLine().Split();
+                ++stu_list[int.Parse(tmp[1])-1, int.Parse(tmp[0])];
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    ans += stu_list[i, j] / maximum;
+                    if (stu_list[i, j] % maximum != 0)
+                    {
+                        ++ans;
+                    }
+                }
+            }
+
+
+            Console.WriteLine(ans);
+
         }
     }
 }
