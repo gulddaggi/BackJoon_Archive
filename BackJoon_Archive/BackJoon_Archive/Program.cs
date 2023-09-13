dba8f3f399010ff102b8426bffdefdb60f63b717
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-09-13 10:20:41 +0900

    10808

--- BackJoon_Archive/BackJoon_Archive/Program.cs
+++ BackJoon_Archive/BackJoon_Archive/Program.cs
@@ -10,6 +10,25 @@ namespace BackJoon_Archive
     {
         static void Main(string[] args)
         {
+            string prob = Console.ReadLine();
+            int[] nums = new int[26];
+
+            for (int i = 97; i < 123; i++)
+			{
+                for (int j = 0; j < prob.Length; j++)
+			    {
+                    if (prob[j] == (char)(i))
+                    {
+                        nums[i-97] += 1;
+                    }
+			    }   
+			}
+
+            for (int i = 0; i < nums.Length; i++)
+			{
+                Console.Write(nums[i]);
+                Console.Write(" ");
+			}
         }
     }
 }
