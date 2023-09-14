0f353a77c8f7a9348886ef6fc9e3fee72345aae3
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-09-14 10:41:26 +0900

    1259

--- BackJoon_Archive/BackJoon_Archive/Program.cs
+++ BackJoon_Archive/BackJoon_Archive/Program.cs
@@ -10,6 +10,40 @@ namespace BackJoon_Archive
     {
         static void Main(string[] args)
         {
+            while (true)
+        	{
+                string prob = Console.ReadLine();
+                if(prob.Length == 1)
+                {
+                    if (prob[0] == '0')
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("yes");
+                        continue;
+                    }
+                }
+
+                int center = (prob.Length / 2) + 1;
+                for (int i = 0; i < center; i++)
+			    {
+                    if (prob[i] != prob[prob.Length-i-1])
+                    {
+                        Console.WriteLine("no");
+                        break;
+                    }
+                    else
+                    {
+                        if(i == center-1) 
+                        {    
+                            Console.WriteLine("yes");
+                            break;
+                        }
+                    }
+                }
+	        }
         }
     }
 }
